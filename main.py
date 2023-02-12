5b8086477c76d80074ed84c923b5c4009fbb2a95
TannyKorry <tannykorry@gmail.com>
2023-02-13 02:03:13 +0300

    revision 1,2

--- main.py
+++ main.py
@@ -5,56 +5,26 @@ import re
 with open("phonebook_raw.csv", 'r', encoding='utf8') as f:
     rows = csv.reader(f, delimiter=",")
     contacts_list = list(rows) # дает список списков из файла
-    # print()
+    # print(contacts_list)
 
 # 1
 def arrange(contacts):
-    new_contacts_list, element = [], []
     for c in contacts:
-        if len(c[0].split()) == 1: # Если "lastname" есть и не содержит больше ничего.
-            element = [c[0] + ', ' + c[1] + ', ' + c[2] + ', ' + c[3] + ', ' + c[4] + ', ' + c[5] + ', ' + c[6]]
-        elif len(c[0].split()) > 1: # Если "lastname" содержит что-то помимо фамилии.
-            s = c[0].split()
-            if len(c[0].split()) == 2:  # Если "lastname" содержит фамилию и имя
-                element = [s[0] + ', ' + s[1] + ', ' + c[2] + ', ' + c[3] + ', ' + c[4] + ', ' + c[5] + ', ' + c[6]]
-            else: # Если "lastname" содержит фамилию, имя и отчество
-                element = [s[0] + ', ' + s[1] + ', ' + s[2] + ', ' + c[3] + ', ' + c[4] + ', ' + c[5] + ', ' + c[6]]
+        item = ' '.join(c[:3]).split(' ')
+        c[:3] = item[:3]
+    return contacts
 
-        if len(c[1].split()) == 1:  # Если "firstname" содержит только имя
-            element = [c[0] + ', ' + c[1] + ', ' + c[2] + ', ' + c[3] + ', ' + c[4] + ', ' + c[5] + ', ' + c[6]]
-        elif len(c[1].split()) > 1: # Если в "firstname" вбито что-то помимо имени:
-            s = c[1].split()
-            if c[0] != "": # При этом если "lastname" есть, а в "firstname" вбиты имя-отчество
-                element = [c[0] + ', ' + s[0] + ', ' + s[1] + ', ' + c[3] + ', ' + c[4] + ', ' + c[5] + ', ' + c[6]]
-            elif c[2] != "": # Или "lastname" нет, "surname" есть, а имя содержит фамилия-имя
-                element = [s[0] + ', ' + s[1] + ', ' + c[2] + ', ' + c[3] + ', ' + c[4] + ', ' + c[5] + ', ' + c[6]]
-            else:
-                element = [s[0] + ', ' + s[1] + ', ' + s[2] + ', ' + c[3] + ', ' + c[4] + ', ' + c[5] + ', ' + c[6]]
-        new_contacts_list.append(element)
-    return new_contacts_list
-
-
-def _split_list(L, n):
-    '''
-    Генератор для формирования списка списков из списка строк
-    '''
-    for i in range(0, len(L), n):
-        yield L[i:i+n]
+#
 
 # 2
 def format_num(contacts):
-    result, cont_list, res = [], [], ''
     for contact in arrange(contacts):
-        for item in contact:
-            pattern = r'(\+7|8)\s*\(?(\d{3})\)?[\s|-]?(\d{3})[\s|-]?(\d{2})[\s|-]?(\d{2})\s*\(?([д][о]?[б]?[.]?\s*\d+)?\)?'
-            substitution = r'+7(\2)\3-\4-\5 \6'
-            res = re.sub(pattern, substitution, item)
-        result.append(res)
-           # Формируем список списков
-    gen = _split_list(result, 1)
-    for item in gen:
-        cont_list.append(item)
-    return cont_list
+        pattern = r'(\+7|8)\s*\(?(\d{3})\)?[\s|-]?(\d{3})[\s|-]?(\d{2})[\s|-]?(\d{2})\s*\(?([д][о]?[б]?[.]?\s*\d+)?\)?'
+        substitution = r'+7(\2)\3-\4-\5 \6'
+        res = re.sub(pattern, substitution, contact[5])
+        contact[5] = res
+    contacts_list.append(contact)
+    return contacts_list
 
 # 3
 def remove_duplicates(contacts):
@@ -70,10 +40,7 @@ def remove_duplicates(contacts):
             print(f'\nitm: {itm}\n')
             print(f"itm[0].split(',')[0]: {itm[0].split(', ')[0]} равен {shablon}?")
             if itm[0].split(',')[0] == shablon:
-                if shablon in item_set:
-                    print('уже есть')
-                else:
-                    item_set += itm
+                item_set += itm
                 pprint(f'item_set: {item_set}')
             else:
                 pass
@@ -107,8 +74,8 @@ def remove_duplicates(contacts):
 
 
 if __name__ == '__main__':
-    # pprint(arrange(contacts_list))
-    # pprint(format_num(contacts_list))
+    # print(arrange(contacts_list))
+    print(format_num(contacts_list))
     print('\n')
-    remove_duplicates(contacts_list)
+    # remove_duplicates(contacts_list)
 
